af8f0e0b437a78f7a1cd75783af20c8f78e61a5b
Gibi0 <kubagra16@wp.pl>
2022-05-28 12:00:19 +0200

    Updating database

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -12,18 +12,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 //        Window window = new Window();
 //        dataBase.getCollection().find().forEach(System.out::println);
-        int i = 1;
         Database dataBase = new Database();
-        String first, second;
-        File file = new File("data.txt");
-        Scanner s = new Scanner(file);
-        s.useDelimiter("[-\n]");
-        while (s.hasNext()) {
-            first = s.next();
-            second = s.next();
-            Document doc = new Document("_id", i).append("English_Word", first).append("Polish_Word", second);
-            dataBase.getCollection().insertOne(doc);
-            i += 1;
-        }
+
     }
 }
