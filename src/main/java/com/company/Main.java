f8d249845f808f40dc0ca04033ccdf9c849c33fd
Gibi0 <kubagra16@wp.pl>
2022-05-28 02:25:23 +0200

    Updating database

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,12 +1,29 @@
 package com.company;
 
 
+import org.bson.Document;
+
+import java.io.*;
+//import java.util.ArrayList
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        Window window = new Window();
+    public static void main(String[] args) throws IOException {
+//        Window window = new Window();
+//        dataBase.getCollection().find().forEach(System.out::println);
+        int i = 1;
         Database dataBase = new Database();
-
-        dataBase.getCollection().find().forEach(System.out::println);
+        String first, second;
+        File file = new File("data.txt");
+        Scanner s = new Scanner(file);
+        s.useDelimiter("[-\n]");
+        while (s.hasNext()) {
+            first = s.next();
+            second = s.next();
+            Document doc = new Document("_id", i).append("English_Word", first).append("Polish_Word", second);
+            dataBase.getCollection().insertOne(doc);
+            i += 1;
+        }
     }
 }
--- src/main/java/com/company/Window.java
+++ src/main/java/com/company/Window.java
@@ -9,5 +9,6 @@ public class Window {
         frame.setSize(800, 600);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Flashcards");
     }
 }
